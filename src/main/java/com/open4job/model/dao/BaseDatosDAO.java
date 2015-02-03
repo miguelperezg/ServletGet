b283bf064cc04f92ac2837153e6260309b5e695b
miguelperezg <miguelperezg1986@gmail.com>
2015-02-03 10:11:29 +0100

    estructura básica del servletGet para taxis

---
+++ src/main/java/com/open4job/model/dao/BaseDatosDAO.java
@@ -0,0 +1,78 @@
+package com.open4job.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class BaseDatosDAO {
+	private String driver;
+	private String url;
+	private String user;
+	private String password;
+	protected Connection connection = null;
+
+	public BaseDatosDAO(String driver, String url, String user, String password) {
+		this.driver = driver;
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Connection abrirConexion() throws SQLException, ClassNotFoundException {
+		Connection conection = null;
+		try {
+			Class.forName(driver);
+			conection = DriverManager.getConnection(url, user, password);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return conection;
+	}
+
+	public void cerrarConexion() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+			}
+		}
+	}
+}
---
+++ src/main/java/com/open4job/model/dao/ParadaTaxiDAO.java
@@ -0,0 +1,88 @@
+package com.open4job.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.open4job.model.vo.ParadaTaxiVO;
+
+public class ParadaTaxiDAO {
+
+	public ParadaTaxiDAO() {
+	}
+
+	// Listado de las paradas de taxi
+
+	public ArrayList<ParadaTaxiVO> getListadoParadaTaxi(Connection conexion)
+			throws SQLException {
+
+		ArrayList<ParadaTaxiVO> lista = new ArrayList<ParadaTaxiVO>();
+
+		PreparedStatement st = conexion
+				.prepareStatement("SELECT * FROM PARADA_TAXI");
+		ResultSet rs = st.executeQuery();
+		while (rs.next()) {
+			lista.add(new ParadaTaxiVO(rs.getInt(1), rs.getString(2), rs
+					.getDate(3), rs.getFloat(4), rs.getFloat(5), rs
+					.getString(6)));
+		}
+		return lista;
+	}
+
+	// Devuelve la parada del taxi por el id
+
+	public ParadaTaxiVO getparadataxi(int id, Connection conexion)
+			throws SQLException {
+		ParadaTaxiVO idLista = null;
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+
+		stmt = conexion
+				.prepareStatement("SELECT * FROM PARADA_TAXI WHERE id = ?");
+		stmt.setInt(1, id);
+		rs = stmt.executeQuery();
+
+		if (rs.next()) {
+			idLista = new ParadaTaxiVO(rs.getInt(1), rs.getString(2),
+					rs.getDate(3), rs.getFloat(4), rs.getFloat(5),
+					rs.getString(6));
+		}
+
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+			}
+		}
+
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+			}
+		}
+		return idLista;
+
+	}
+
+	public void insertValues(Connection conexion) throws SQLException {
+		java.util.Date utilDate = new java.util.Date();
+		float corx = 14.3f;
+		float cory = 14.3f;
+		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+		PreparedStatement stmt = null;
+		stmt = conexion
+				.prepareStatement("INSERT INTO PARADA_TAXI(ID, TITLE, LASTUPDATE, COR_X, COR_Y, ICON) "
+						+ "	VALUES (?, ?, ?, ?, ?, ?)");
+		stmt.setInt(1, 6);
+		stmt.setString(2, "ROMAREDA");
+		stmt.setDate(3, sqlDate);
+		stmt.setFloat(4, corx);
+		stmt.setFloat(5, cory);
+		stmt.setString(6, "circulo");
+		stmt.executeUpdate();
+	}
+
+}
